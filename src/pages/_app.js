de27914bfaf4fbbf169bd050e6893bee1cc99c22
Saad Srabon <saadsrabon2@gmail.com>
2023-08-23 00:08:00 +0600

    files added

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import Navbar from "@/components/Layout/Navbar";
+import { SessionProvider } from "next-auth/react"
+export default function App({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session}>
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+    </SessionProvider>
+  );
+}
